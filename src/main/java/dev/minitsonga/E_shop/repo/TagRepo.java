a8110f32c82c202d9e2de2b3df9e5ef829317c82
Julien <71793112+Minitsonga@users.noreply.github.com>
2025-01-09 14:27:39 +0100

    Added Tag Repository

---
+++ src/main/java/dev/minitsonga/E_shop/repo/TagRepo.java
@@ -0,0 +1,10 @@
+﻿package dev.minitsonga.E_shop.repo;
+
+import dev.minitsonga.E_shop.model.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepo extends JpaRepository<Tag, String>
+{
+}
