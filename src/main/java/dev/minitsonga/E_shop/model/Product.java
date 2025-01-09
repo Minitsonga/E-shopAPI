6301d31c8b6d58c572d871fd0b57b87f784807ef
Julien <71793112+Minitsonga@users.noreply.github.com>
2025-01-09 03:11:30 +0100

    Added ProductRepo

--- src/main/java/dev/minitsonga/E_shop/model/Product.java
+++ src/main/java/dev/minitsonga/E_shop/model/Product.java
@@ -2,11 +2,11 @@
 
 import jakarta.persistence.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
- @Table(name = "products")
+@Table(name = "products")
 public class Product
 {
     @Id
@@ -15,22 +15,101 @@ public class Product
 
     private String name;
     private String description;
-    private Float price;
+    private String imageUrl;
+    private Double price;
     private Integer stockQuantity;
 
-    @ElementCollection // Permet de stocker une collection d'éléments simples.
+    @ElementCollection(targetClass = Tag.class)
     @CollectionTable(name = "product_tags", joinColumns = @JoinColumn(name = "product_id"))
-    @Column(name = "tags")
-    private List<String> tags = new ArrayList<String>();
-
+    @Enumerated(EnumType.STRING) // Stocke les valeurs Enum sous forme de texte
+    @Column(name = "tag")
+    private Set<Tag> tags = new HashSet<>();
 
     // Getters et Setters
-    public List<String> getTags() {
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public String getImageUrl()
+    {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl)
+    {
+        this.imageUrl = imageUrl;
+    }
+
+
+    public Double getPrice()
+    {
+        return price;
+    }
+
+    public void setPrice(Double price)
+    {
+        this.price = price;
+    }
+
+    public Integer getStockQuantity()
+    {
+        return stockQuantity;
+    }
+
+    public void getStockQuantity(Integer stock)
+    {
+        this.stockQuantity = stock;
+    }
+
+    public Set<Tag> getTags()
+    {
         return tags;
     }
 
-    public void setTags(List<String> tags) {
+    public void setTags(Set<Tag> tags)
+    {
         this.tags = tags;
     }
 
+
+    public enum Tag
+    {
+        HIGHTECH,
+        PROMOTION,
+        BESTSELLER,
+        SOLDES,
+        NOUVEAU,
+        LIVRE,
+        EPICERIE,
+        CUISINE,
+        SPORT
+    }
+
 }
+
---
+++ src/main/java/dev/minitsonga/E_shop/repo/ProductRepo.java
@@ -0,0 +1,21 @@
+﻿package dev.minitsonga.E_shop.repo;
+
+import dev.minitsonga.E_shop.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Set;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product, Long>
+{
+    // Requête personnalisée pour filtrer par tags
+    @Query("SELECT DISTINCT p FROM Product p JOIN p.tags t WHERE t IN :tags")
+    List<Product> findProductByTags(@Param("tags") Set<Product.Tag> tags);
+
+    void deleteProductById(Long id);
+
+}
