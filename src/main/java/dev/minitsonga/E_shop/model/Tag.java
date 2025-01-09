68184281396d2d092e669771ca9537d07d48f911
Julien <71793112+Minitsonga@users.noreply.github.com>
2025-01-09 14:27:26 +0100

    Added Tag Entity

---
+++ src/main/java/dev/minitsonga/E_shop/model/Tag.java
@@ -0,0 +1,36 @@
+﻿package dev.minitsonga.E_shop.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tags")
+public class Tag
+{
+    @Id
+    @Column(nullable = false, unique = true)
+    private String name;
+
+
+    public Tag()
+    {
+    }
+
+    public Tag(String name)
+    {
+        this.name = name;
+    }
+
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
