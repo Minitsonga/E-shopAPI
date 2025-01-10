e26aa51dbda070a34335ec5428c79847a90839a6
Julien <71793112+Minitsonga@users.noreply.github.com>
2025-01-10 03:37:08 +0100

    Added TagService

---
+++ src/main/java/dev/minitsonga/E_shop/service/TagService.java
@@ -0,0 +1,37 @@
+﻿package dev.minitsonga.E_shop.service;
+
+
+import dev.minitsonga.E_shop.model.Tag;
+import dev.minitsonga.E_shop.repo.TagRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TagService
+{
+    private final TagRepo tagRepo;
+
+    public TagService(TagRepo tagRepo)
+    {
+        this.tagRepo = tagRepo;
+    }
+
+
+    public List<Tag> getAllTags()
+    {
+        return tagRepo.findAll();
+    }
+
+    public Tag createTag(String name)
+    {
+        if(tagRepo.existsById(name)) throw new IllegalArgumentException("Tag already exists!");
+        return tagRepo.save(new Tag(name));
+    }
+
+    public void deleteTag(String name)
+    {
+        tagRepo.deleteById(name);
+    }
+}
