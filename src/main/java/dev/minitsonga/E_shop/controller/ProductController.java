176026e995448f785627314da8484b5090af536b
Julien <71793112+Minitsonga@users.noreply.github.com>
2025-01-10 03:37:23 +0100

    Create ProductController.java

---
+++ src/main/java/dev/minitsonga/E_shop/controller/ProductController.java
@@ -0,0 +1,10 @@
+﻿package dev.minitsonga.E_shop.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController
+{
+}
