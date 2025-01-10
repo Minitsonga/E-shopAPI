9f0ad9c2c8378421396ccd4e8f512122276c2a96
Julien <71793112+Minitsonga@users.noreply.github.com>
2025-01-10 03:37:21 +0100

    Create ProductService.java

---
+++ src/main/java/dev/minitsonga/E_shop/service/ProductService.java
@@ -0,0 +1,45 @@
+﻿package dev.minitsonga.E_shop.service;
+
+import dev.minitsonga.E_shop.model.Product;
+import dev.minitsonga.E_shop.model.Tag;
+import dev.minitsonga.E_shop.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class ProductService
+{
+    private final ProductRepo productRepo;
+
+    public ProductService (ProductRepo productRepo)
+    {
+        this.productRepo = productRepo;
+    }
+
+
+    public Product createProduct(String name, String description, String imageUrl, Double price, Integer stockQuantity, Set<Tag> tags)
+    {
+        return productRepo.save(new Product(name, description, imageUrl, price, stockQuantity, tags));
+    }
+
+    public Product UpdateProduct(Product product)
+    {
+        return productRepo.save(product);
+    }
+
+    public void deleteProduct(Long id)
+    {
+        productRepo.deleteById(id);
+    }
+
+
+    public List<Product> getAllProducts()
+    {
+        return productRepo.findAll();
+    }
+
+
+}
