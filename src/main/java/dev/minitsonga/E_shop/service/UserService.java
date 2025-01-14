03e4290255f0018d678160f341bd6a589c466920
Julien <71793112+Minitsonga@users.noreply.github.com>
2025-01-14 04:06:39 +0100

    Update UserService.java

--- src/main/java/dev/minitsonga/E_shop/service/UserService.java
+++ src/main/java/dev/minitsonga/E_shop/service/UserService.java
@@ -1,47 +1,43 @@
 ﻿package dev.minitsonga.E_shop.service;
 
+import dev.minitsonga.E_shop.config.SecurityConfig;
 import dev.minitsonga.E_shop.model.Role;
 import dev.minitsonga.E_shop.model.User;
 import dev.minitsonga.E_shop.repo.RoleRepo;
 import dev.minitsonga.E_shop.repo.UserRepo;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
-public class UserService
-{
+public class UserService {
     private final UserRepo userRepo;
 
     private final RoleRepo roleRepo;
 
-    //private final PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
-
-    public UserService(UserRepo userRepo/*, PasswordEncoder passwordEncoder*/, RoleRepo roleRepo)
-    {
+    public UserService(UserRepo userRepo, PasswordEncoder passwordEncoder, RoleRepo roleRepo) {
         this.userRepo = userRepo;
-        //this.passwordEncoder = passwordEncoder;
+        this.passwordEncoder = passwordEncoder;
         this.roleRepo = roleRepo;
     }
 
-    public User createUser(User user, Set<String> roleNames)
-    {
+    public User createUser(User user) {
         Role defaultRole = findRoleByName("USER");
         user.getRoles().add(defaultRole);
-        //user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
 
         return userRepo.save(user);
 
     }
 
-    public User updateUserRoles(String username, Set<String> roleNames)
-    {
+    public User updateUserRoles(String username, Set<String> roleNames) {
         User user = findByUserName(username);
         Set<Role> newRoles = roleNames.stream().map(this::findRoleByName).collect(Collectors.toSet());
 
@@ -50,26 +46,26 @@ public class UserService
 
     }
 
-    public User findByUserName(String username)
-    {
-        return userRepo.findByUsername(username).orElseThrow(() -> new RuntimeException("User not found : " + username));
+    public User findByUserName(String username) {
+        return userRepo.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User not found : " + username));
     }
 
-    public User findByEmail(String email)
-    {
+    public User findByEmail(String email) {
         return userRepo.findByEmail(email).orElseThrow(() -> new RuntimeException("User not found : " + email));
     }
 
-    public List<User> findByFirstNameAndLastName(String firstName, String lastName)
-    {
-        return userRepo.findByFirstNameAndLastName(firstName, lastName).orElseThrow(() -> new RuntimeException("User not found : " + firstName + " " + lastName));
+    public List<User> findByFirstNameAndLastName(String firstName, String lastName) {
+        return userRepo.findByFirstNameAndLastName(firstName, lastName)
+                .orElseThrow(() -> new RuntimeException("User not found : " + firstName + " " + lastName));
     }
 
-
-    public Role findRoleByName(String roleName)
-    {
+    public Role findRoleByName(String roleName) {
         return roleRepo.findByName(roleName).orElseThrow(() -> new RuntimeException("Role not found : " + roleName));
     }
 
+    public List<User> getAllUsers() {
+        return userRepo.findAll();
+    }
 
 }
