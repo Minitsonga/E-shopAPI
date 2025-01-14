436c14568bd7050dc767a12bc7a2e1bec52aff63
Julien <71793112+Minitsonga@users.noreply.github.com>
2025-01-14 03:31:11 +0100

    Fixed Role Repo

--- src/main/java/dev/minitsonga/E_shop/repo/RoleRepo.java
+++ src/main/java/dev/minitsonga/E_shop/repo/RoleRepo.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface RoleRepo extends JpaRepository<Role, Long>
+public interface RoleRepo extends JpaRepository<Role, String>
 {
     Optional<Role> findByName(String name);
 
