c1a2c29707ce0a74eae322121b7d9c076383d928
Julien <71793112+Minitsonga@users.noreply.github.com>
2025-01-15 03:54:06 +0100

    Update SecurityConfig.java
    Adding Authentication

--- src/main/java/dev/minitsonga/E_shop/config/SecurityConfig.java
+++ src/main/java/dev/minitsonga/E_shop/config/SecurityConfig.java
@@ -1,15 +1,50 @@
 ﻿package dev.minitsonga.E_shop.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import dev.minitsonga.E_shop.service.UserService;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfiguration {
+
+    private final UserService userService;
+
+    public SecurityConfig(UserService userService) {
+        this.userService = userService;
+    }
 
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(15);
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    // Configuration des règles de sécurité HTTP
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable()) // Désactiver CSRF (à activer pour les formulaires)
+                .authorizeHttpRequests(auth -> auth
+                                .requestMatchers("/api/users/**").hasRole("ADMIN") // Restreindre l'accès aux admins
+                                .requestMatchers("/api/products/**").authenticated() // Authentification requise
+                                .anyRequest().permitAll() // Autoriser toutes les autres requêtes
+                )
+                .httpBasic(withDefaults()); // Utiliser HTTP Basic pour l'authentification (facile pour Postman)
+        return http.build();
     }
 }
