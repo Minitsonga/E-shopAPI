30a8e6058e15562c5f671f8ab05d3f8d451fd54a
Julien <71793112+Minitsonga@users.noreply.github.com>
2025-01-15 03:55:07 +0100

    Update TagService.java

--- src/main/java/dev/minitsonga/E_shop/service/TagService.java
+++ src/main/java/dev/minitsonga/E_shop/service/TagService.java
@@ -1,6 +1,5 @@
 ﻿package dev.minitsonga.E_shop.service;
 
-
 import dev.minitsonga.E_shop.model.Tag;
 import dev.minitsonga.E_shop.repo.TagRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,29 +8,26 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class TagService
-{
+public class TagService {
     private final TagRepo tagRepo;
 
-    public TagService(TagRepo tagRepo)
-    {
+    public TagService(TagRepo tagRepo) {
         this.tagRepo = tagRepo;
     }
 
-
-    public List<Tag> getAllTags()
-    {
+    public List<Tag> getAllTags() {
         return tagRepo.findAll();
     }
 
-    public Tag createTag(String name)
-    {
-        if(tagRepo.existsById(name)) throw new IllegalArgumentException("Tag already exists!");
+    public Tag createTag(String name) {
+        if (tagRepo.existsById(name))
+            throw new IllegalArgumentException("Tag already exists ! : " + name);
         return tagRepo.save(new Tag(name));
     }
 
-    public void deleteTag(String name)
-    {
+    public void deleteTag(String name) {
+        if (!tagRepo.existsById(name))
+            throw new RuntimeException("Tag doesn't exists ! : " + name);
         tagRepo.deleteById(name);
     }
 }
