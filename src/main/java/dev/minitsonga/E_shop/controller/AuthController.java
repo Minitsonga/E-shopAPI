a04cfc2616bab19f915e22a7f9d661c91b747055
Julien <71793112+Minitsonga@users.noreply.github.com>
2025-01-15 03:54:11 +0100

    Create AuthController.java

---
+++ src/main/java/dev/minitsonga/E_shop/controller/AuthController.java
@@ -0,0 +1,10 @@
+package dev.minitsonga.E_shop.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+}
