debdb11ad024c99f24ff3daa067bb71ebc770048
Julien <71793112+Minitsonga@users.noreply.github.com>
2025-01-22 01:52:55 +0100

    Create UserDTOMapper.java

---
+++ src/main/java/dev/minitsonga/E_shop/model/UserDTOMapper.java
@@ -0,0 +1,27 @@
+package dev.minitsonga.E_shop.model;
+
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDTOMapper implements Function<User, UserDTO> {
+
+    @Override
+    public UserDTO apply(User user) {
+
+        return new UserDTO(
+                user.getUsername(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getAge(),
+                user.getEmail(),
+                user.getPhone(),
+                user.getRoles().stream().map(r -> r.getRole()).collect(Collectors.toList()),
+                new AddressDTO(user.getAddress(), user.getCity(), user.getZip()));
+
+    }
+
+}
