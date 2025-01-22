54a3e5d60933ff6ae41aba8128b6728037c780f5
Julien <71793112+Minitsonga@users.noreply.github.com>
2025-01-22 01:52:58 +0100

    Update UserDTO.java

--- src/main/java/dev/minitsonga/E_shop/model/UserDTO.java
+++ src/main/java/dev/minitsonga/E_shop/model/UserDTO.java
@@ -1,11 +1,17 @@
 package dev.minitsonga.E_shop.model;
 
+import java.util.List;
+
 public record UserDTO(
         String username,
         String firstName,
         String lastName,
+        Integer age,
         String email,
         String phone,
+        List<String> roles,
         AddressDTO addressDTO) {
+
+   
 
 }
