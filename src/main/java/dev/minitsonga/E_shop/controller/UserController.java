b32da394b3937f5e05edb176fef7a3653e4be1a1
Julien <71793112+Minitsonga@users.noreply.github.com>
2025-01-22 01:53:05 +0100

    Update UserController.java

--- src/main/java/dev/minitsonga/E_shop/controller/UserController.java
+++ src/main/java/dev/minitsonga/E_shop/controller/UserController.java
@@ -4,10 +4,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.minitsonga.E_shop.model.User;
+import dev.minitsonga.E_shop.model.UserDTO;
+import dev.minitsonga.E_shop.model.UserDTOMapper;
+import dev.minitsonga.E_shop.model.UserSignUpDTO;
 import dev.minitsonga.E_shop.service.UserService;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,30 +29,32 @@ import org.springframework.web.bind.annotation.PutMapping;
 public class UserController {
 
     private final UserService userService;
+    private final UserDTOMapper UserDTOMapper;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, UserDTOMapper userDTOMapper) {
         this.userService = userService;
+        this.UserDTOMapper = userDTOMapper;
     }
     
     @GetMapping
-    public ResponseEntity<List<User>> getAllUsers(@RequestParam String username) {
-        return ResponseEntity.ok(userService.getAllUsers());
+    public ResponseEntity<List<UserDTO>> getAllUsers(@RequestParam String username) {
+        return ResponseEntity.ok(userService.findAllUsers().stream().map(UserDTOMapper).collect(Collectors.toList()));
     }
     @PostMapping(path = "/add")
-    public ResponseEntity<User> registerUser(@RequestBody User user) {
+    public ResponseEntity<User> registerUser(@RequestBody UserSignUpDTO userSignUpDTO) {
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(user));
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(userSignUpDTO));
     }
 
     @PutMapping(path = "/{username}/roles/update")
-    public ResponseEntity<User> updateUserRoles(@PathVariable String username, @RequestBody Set<String> rolesName) {
+    public ResponseEntity<User> updateUserRoles(@PathVariable String username, @RequestBody Set<String> newRolesName) {
 
-        return ResponseEntity.ok(userService.updateUserRoles(username, rolesName));
+        return ResponseEntity.ok(userService.updateUserRoles(username, newRolesName));
     }
 
     @GetMapping(path = "/{username}")
     public ResponseEntity<User> getUserByUsername(@RequestParam String username) {
-        return ResponseEntity.ok(userService.getUserByUsername(username));
+        return ResponseEntity.ok(userService.findUserByUsername(username));
     }
 
 
