48ab2f7553528bb221c2c6ccb59c45b7a36f882b
Julien <71793112+Minitsonga@users.noreply.github.com>
2025-01-22 12:55:38 +0100

    Update Mapping with id path & adding password Update

--- src/main/java/dev/minitsonga/E_shop/controller/UserController.java
+++ src/main/java/dev/minitsonga/E_shop/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import dev.minitsonga.E_shop.model.User;
 import dev.minitsonga.E_shop.model.UserDTO;
 import dev.minitsonga.E_shop.model.UserDTOMapper;
+import dev.minitsonga.E_shop.model.UserPasswordDTO;
 import dev.minitsonga.E_shop.model.UserSignUpDTO;
 import dev.minitsonga.E_shop.service.UserService;
 
@@ -19,44 +20,67 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
-
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService;
-    private final UserDTOMapper UserDTOMapper;
+    private final UserDTOMapper userDTOMapper;
 
     public UserController(UserService userService, UserDTOMapper userDTOMapper) {
         this.userService = userService;
-        this.UserDTOMapper = userDTOMapper;
+        this.userDTOMapper = userDTOMapper;
     }
-    
+
     @GetMapping
     public ResponseEntity<List<UserDTO>> getAllUsers(@RequestParam String username) {
-        return ResponseEntity.ok(userService.findAllUsers().stream().map(UserDTOMapper).collect(Collectors.toList()));
+        List<UserDTO> users = userService.findAllUsers().stream()
+                .map(userDTOMapper::apply)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(users);
     }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<UserDTO> getUserByUsername(@PathVariable String username) {
+        User user = userService.findUserByUsername(username);
+        return ResponseEntity.ok(userDTOMapper.apply(user));
+    }
+
     @PostMapping(path = "/add")
-    public ResponseEntity<User> registerUser(@RequestBody UserSignUpDTO userSignUpDTO) {
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(userSignUpDTO));
+    public ResponseEntity<UserDTO> registerUser(@RequestBody UserSignUpDTO userSignUpDTO) {
+        User user = userService.createUser(userSignUpDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userDTOMapper.apply(user));
     }
 
-    @PutMapping(path = "/{username}/roles/update")
-    public ResponseEntity<User> updateUserRoles(@PathVariable String username, @RequestBody Set<String> newRolesName) {
-
-        return ResponseEntity.ok(userService.updateUserRoles(username, newRolesName));
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable Long id, @RequestBody Record dto) {
+        User updatedUser = userService.updateUserDetails(id, dto);
+        return ResponseEntity.ok(userDTOMapper.apply(updatedUser));
     }
 
-    @GetMapping(path = "/{username}")
-    public ResponseEntity<User> getUserByUsername(@RequestParam String username) {
-        return ResponseEntity.ok(userService.findUserByUsername(username));
+    @PutMapping("/{id}/password")
+    public ResponseEntity<UserDTO> updatePasswordUser(@PathVariable Long id, @RequestBody UserPasswordDTO userPasswordDTO) {
+        User updatedUser = userService.updateUserDetails(id, userPasswordDTO);
+        return ResponseEntity.ok(userDTOMapper.apply(updatedUser));
     }
 
+    @PutMapping("/{id}/roles")
+    public ResponseEntity<UserDTO> updateUserRoles(
+            @PathVariable Long id,
+            @RequestBody Set<String> roleNames) {
+        User updatedUser = userService.updateUserRoles(id, roleNames);
+        return ResponseEntity.ok(userDTOMapper.apply(updatedUser));
+    }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUserById(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
