b2d1745456815ad69ef33c3837710ee9c937d741
Julien <71793112+Minitsonga@users.noreply.github.com>
2025-01-22 12:52:59 +0100

    Update UserMapper.java

--- src/main/java/dev/minitsonga/E_shop/model/UserMapper.java
+++ src/main/java/dev/minitsonga/E_shop/model/UserMapper.java
@@ -8,32 +8,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserMapper implements Function<Record, User> {
-
- 
-    // private User mapFromUserSignUpDTO(UserSignUpDTO userSignUpDTO) {
-    // User user = new User();
-    // user.setUsername(userSignUpDTO.username());
-    // user.setEmail(userSignUpDTO.email());
-    // user.setPassword(userSignUpDTO.password());
-    // // Rôle par défaut
-    // Role defaultRole = new Role();
-    // defaultRole.setRole("USER");
-    // user.setRoles(Set.of(defaultRole));
-    // return user;
-    // }
-
-    // public User mapFromUserProfileDTO(UserProfileDTO userProfileDTO) {
-    // User user = new User();
-    // user.setUsername(userSignUpDTO.username());
-    // user.setEmail(userSignUpDTO.email());
-    // user.setPassword(userSignUpDTO.password());
-    // // Rôle par défaut
-    // Role defaultRole = new Role();
-    // defaultRole.setRole("USER");
-    // user.setRoles(Set.of(defaultRole));
-    // return user;
-    // }
-
     @Override
     public User apply(Record r) {
 
@@ -42,27 +16,12 @@ public class UserMapper implements Function<Record, User> {
         user.setUsername(userSignUpDTO.username());
         user.setEmail(userSignUpDTO.email());
         user.setPassword(userSignUpDTO.password());
+        
         // Rôle par défaut
         Role defaultRole = new Role();
         defaultRole.setRole("USER");
         user.setRoles(Set.of(defaultRole));
         return user;
 
-        // switch (r) {
-        // case UserSignUpDTO userSignUpDTO:
-
-        // break;
-        // case UserDTO userDTO:
-        // return mapFromUserDTO(userDTO);
-        // break;
-        // case UserProfileDTO userProfileDTO:
-        // return mapFromUserProfileDTO(userProfileDTO);
-        // break;
-
-        // default:
-        // throw new UnsupportedOperationException("Unsupported Record type: " +
-        // r.getClass().getName());
-        // break;
-        // }
     }
 }
