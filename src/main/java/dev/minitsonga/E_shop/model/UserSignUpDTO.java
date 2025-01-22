672fd9176b103ee1bc2b4d7f125e4d4ce0976c68
Julien <71793112+Minitsonga@users.noreply.github.com>
2025-01-22 01:52:47 +0100

    Create UserSignUpDTO.java

---
+++ src/main/java/dev/minitsonga/E_shop/model/UserSignUpDTO.java
@@ -0,0 +1,21 @@
+package dev.minitsonga.E_shop.model;
+
+import java.util.Set;
+
+public record UserSignUpDTO(
+        String username,
+        String email,
+        String password) {
+
+    public User convertToUser(UserSignUpDTO userSignUpDTO) {
+        User user = new User();
+        user.setUsername(userSignUpDTO.username());
+        user.setEmail(userSignUpDTO.email());
+        user.setPassword(userSignUpDTO.password());
+        // Rôle par défaut
+        Role defaultRole = new Role();
+        defaultRole.setRole("USER");
+        user.setRoles(Set.of(defaultRole));
+        return user;
+    }
+}
