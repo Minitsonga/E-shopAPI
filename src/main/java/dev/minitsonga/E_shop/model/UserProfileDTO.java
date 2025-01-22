d503157fa86a1f628a61b85ef84766b481031a5c
Julien <71793112+Minitsonga@users.noreply.github.com>
2025-01-22 12:52:43 +0100

    Added Id variable

--- src/main/java/dev/minitsonga/E_shop/model/UserProfileDTO.java
+++ src/main/java/dev/minitsonga/E_shop/model/UserProfileDTO.java
@@ -1,10 +1,12 @@
 package dev.minitsonga.E_shop.model;
 
 public record UserProfileDTO(
-                String username,
-                String gender,
-                String firstName,
-                String lastName,
-                Integer age,
-                AddressDTO addressDTO) {
+        Long id,
+        String username,
+        String gender,
+        String firstName,
+        String lastName,
+        Integer age,
+        String email,
+        AddressDTO addressDTO) {
 }
