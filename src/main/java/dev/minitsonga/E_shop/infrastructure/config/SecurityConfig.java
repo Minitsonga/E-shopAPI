fa1702f4b2b99d288c6659ec997fd29e1d091afa
Julien <71793112+Minitsonga@users.noreply.github.com>
2025-01-31 03:25:09 +0100

    Update SecurityConfig.java
    Add session Management & filter with jwtFilter

--- src/main/java/dev/minitsonga/E_shop/infrastructure/config/SecurityConfig.java
+++ src/main/java/dev/minitsonga/E_shop/infrastructure/config/SecurityConfig.java
@@ -9,21 +9,24 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import dev.minitsonga.E_shop.infrastructure.security.CustomUserDetailsService;
-import jakarta.security.auth.message.config.AuthConfigProvider;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     private final CustomUserDetailsService userDetailsService;
+    private final JWTFilter jwtFilter;
 
-    public SecurityConfig(CustomUserDetailsService userDetailsService) {
+    public SecurityConfig(CustomUserDetailsService userDetailsService, JWTFilter jwtFilter) {
         this.userDetailsService = userDetailsService;
+        this.jwtFilter = jwtFilter;
     }
 
     @Bean
@@ -39,22 +42,24 @@ public class SecurityConfig {
         return provider;
     }
 
-     @Bean
-     AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-         return config.getAuthenticationManager();
-     }
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
 
     // Configuration des règles de sécurité HTTP
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable()) // Désactiver CSRF (à activer pour les formulaires)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/users/signup").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/users/**").hasAuthority("ROLE_ADMIN") // Restreindre l'accès aux admins
                         .requestMatchers("/api/products/**").authenticated() // Authentification requise
                         .anyRequest().permitAll() // Autoriser toutes les autres requêtes
                 )
-                .httpBasic(withDefaults()); // Utiliser HTTP Basic pour l'authentification (facile pour Postman)
+                .httpBasic(withDefaults()) // Utiliser HTTP Basic pour l'authentification (facile pour Postman)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
