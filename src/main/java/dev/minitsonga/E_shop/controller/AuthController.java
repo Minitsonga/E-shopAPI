5aea2b1132031fb8fd93285bdca7d1ed070d3ee2
Julien <71793112+Minitsonga@users.noreply.github.com>
2025-01-31 04:24:11 +0100

    Update AuthController.java

--- src/main/java/dev/minitsonga/E_shop/controller/AuthController.java
+++ src/main/java/dev/minitsonga/E_shop/controller/AuthController.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -65,7 +66,6 @@ public class AuthController {
         }
 
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("Error", "User not found"));
-
     }
 
 }
